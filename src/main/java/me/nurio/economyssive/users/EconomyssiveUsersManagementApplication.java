793c1caa5050950485662eb313dae730294522a0
Katakurinna <alesagustin@gmail.com>
2020-10-27 20:51:54 +0100

    (#1) Initial commit

---
+++ src/main/java/me/nurio/economyssive/users/EconomyssiveUsersManagementApplication.java
@@ -0,0 +1,13 @@
+package me.nurio.economyssive.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomyssiveUsersManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomyssiveUsersManagementApplication.class, args);
+	}
+
+}
